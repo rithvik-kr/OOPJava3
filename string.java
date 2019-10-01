6fa174d9308912769041197d5b782036f67470cf
rithvik-kr <55342634+rithvik-kr@users.noreply.github.com>
2019-10-01 11:29:43 +0530

    Add files via upload

--- string.java
+++ string.java
@@ -1,57 +1,58 @@
 import java.util.Scanner;
 import java.util.*;
 
-class string 
+class string
 {
 	Scanner sc=new Scanner(System.in);
+	
 	void extract(String text)
 	{
-		
-		System.out.println("Enter the Amount of Characters to be Extracted from the String : ");
-		int m = sc.nextInt();
-		System.out.println("Enter the Position of Extraction : ");
+		System.out.println("Enter the position : ");
 		int n=sc.nextInt();
-		System.out.println(text.substring(n, n+m));
+		System.out.println("Enter the Number of characters to be Extracted : ");
+		int m=sc.nextInt();
+		System.out.println("text.substring(n, n+m)");
 	}
-
+	
 	void occ(String text)
 	{
-		String[] tokens = text.split(" ");
-		int wrc=1;
-
-		for(int i=0;i<tokens.length;i++)
+		String[] token = text.split(" ");
+		int n=1;
+		
+		for(int i=0; i<token.length; i++)
 		{
-			for(int j=i+1;j<tokens.length;j++)
+			for(int j=i+1; j<token.length; j++)
 			{
-				if(tokens[i].equals(tokens[j]))
+				if(token[i].equals(token[j]))
 				{
-					wrc=wrc+1;
-					tokens[j]="0";
+					n=n+1;
+					token[j]="0";
 				}
 			}
-			if(tokens[i]!="0")
-			System.out.println(tokens[i]+" : "+wrc);
-			wrc=1;
+			if(token[i]!="0")
+			{
+				System.out.println(token[i]+" : "+n);
+				n=1;
+			}
 		}
 	}
-		
+	
 	void replace(String text)
 	{
-		String txt;
-		System.out.println("Enter the Word You Want to Replace : ");
-		txt = sc.next();
-		StringBuilder sb=new StringBuilder();
-		String[] tokens = text.split(" ");
-		
-		System.out.println("Enter the Word to Replace With : ");
+		String[] token = text.split(" ");
+		System.out.println("Enter the Word you want to replace : ");
+		String txt=sc.next();
+		System.out.println("Enter the Word to Replace Wtih : ");
 		String t=sc.next();
-		for(int i=0;i<tokens.length;i++)
+		StringBuilder sb=new StringBuilder();
+		
+		for(int i=0; i<token.length; i++)
 		{
-			if(txt.equals(tokens[i]))
-			{
-				tokens[i]=t;
-			}
-			sb.append(tokens[i]);
+			if(token[i].equals(txt))
+				{
+					token[i]=t;		
+				}
+			sb.append(token[i]);
 			sb.append(" ");
 		}
 		System.out.println(sb.toString());
@@ -59,42 +60,88 @@ class string
 	
 	void rearrange(String text)
 	{
-		String txt=text;
-		for(int i = 1; i<txt.length; i++)
-		{
-			System.out.println(text.charAt(i));
-		}
-	}			
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	public static void main(String args[])
+		char[] temp = text.toCharArray();
+		Arrays.sort(temp);
+		StringBuilder sb=new StringBuilder();
+		for(int i=0; i<temp.length; i++)
+		sb.append(temp[i]);
+		System.out.println("The Sorted String is : "+sb.toString());
+		
+	}
+	
+	void compare(String text)
 	{
-		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Second String : ");
+		String t = sc.nextLine();
+		if(text.equalsIgnoreCase(t))
+		{
+			System.out.println("The Strings Are Equal : ");	
+		}
+		else
+		System.out.println("The Strings are Not Equal : ");
+	}
+
+	void join(String text)
+	{
+		System.out.println("Enter the String to be Joined : ");
+		String t = sc.nextLine();
+		StringBuilder sb=new StringBuilder();
+		sb.append(text).append(" ").append(t);
+		System.out.println("The Concatenated String is : "+sb.toString());
+	}	
+
+
+
+	public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
 		string s=new string();
 		System.out.println("Enter the String : ");
 		String text = sc.nextLine();
-		System.out.println("The Entered String is : " + text);
-		//s.extract(text);
-		//s.occ(text);
-		//s.replace(text);
-		s.replace(text);
+		int ch;
+		do
+		{
+			System.out.println("Main Menu\n 1. Extract\n 2. Occurrence\n 3. Replace\n 4. Rearrange\n 5. Compare\n 6. Concatenate\n 7. Exit\nYour Choice : ");
+			ch=sc.nextInt();
+			switch(ch)
+			{
+				case 1:
+					s.extract(text);
+					break;
+				case 2:
+					s.occ(text);
+					break;
+				case 3:
+					s.replace(text);
+					break;
+				case 4:
+					s.rearrange(text);
+					break;
+				case 5:
+					s.compare(text);
+					break;
+				case 6:
+					s.join(text);
+					break;
+			}			
+		}
+		while(ch!=7);
 	}
 }
+
+			
+					
+
+
+
+
+
+
+
+
+
+
+
+
+
+
